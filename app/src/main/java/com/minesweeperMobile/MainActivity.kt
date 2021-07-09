6f3065eddd76a41a9eeb989930e04ccfb8548df0
Josh <Josh_Rose@alumni.baylor.edu>
2021-07-09 14:59:28 +0300

    Readded the status bar

--- app/src/main/java/com/minesweeperMobile/MainActivity.kt
+++ app/src/main/java/com/minesweeperMobile/MainActivity.kt
@@ -16,9 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) window.insetsController?.hide(WindowInsets.Type.statusBars())
-        else window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
